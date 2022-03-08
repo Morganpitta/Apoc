478044ddb8bd849b99e2ab2556df4dd4b9aabaf9
Morganpitta <89264325+Morganpitta@users.noreply.github.com>
2022-03-08 17:36:48 +0000

    fixng slabs and stairs and stuff

--- src/main/java/net/fabricmc/morgan/mixin/entity/EntityMixin.java
+++ src/main/java/net/fabricmc/morgan/mixin/entity/EntityMixin.java
@@ -309,30 +309,35 @@ public abstract class EntityMixin  implements Nameable, EntityLike, CommandOutpu
     }
 
     @Inject(method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;",at = @At("HEAD"),cancellable = true)
-    private void adjustMovementForCollisionsUpsideDown(Vec3d movement, CallbackInfoReturnable<Vec3d> cir) {
-         if (this.upsideDownGravity()) {
-             boolean bl4;
-             Box box = this.getBoundingBox();
-             List<VoxelShape> list = this.world.getEntityCollisions((Entity) (Object) this, box.stretch(movement));
-             Vec3d vec3d = movement.lengthSquared() == 0.0 ? movement : Entity.adjustMovementForCollisions((Entity) (Object) this, movement, box, this.world, list);
-             boolean bl = movement.x != vec3d.x;
-             boolean bl2 = movement.y != vec3d.y;
-             boolean bl3 = movement.z != vec3d.z;
-             boolean bl5 = bl4 = this.onGround || bl2 && movement.y > 0.0;
-             if (this.stepHeight > 0.0f && bl4 && (bl || bl3)) {
-                 Vec3d vec3d4;
-                 Vec3d vec3d2 = Entity.adjustMovementForCollisions((Entity) (Object) this, new Vec3d(movement.x, -this.stepHeight, movement.z), box, this.world, list);
-                 Vec3d vec3d3 = Entity.adjustMovementForCollisions((Entity) (Object) this, new Vec3d(0.0, -this.stepHeight, 0.0), box.stretch(movement.x, 0.0, movement.z), this.world, list);
-                 if (vec3d3.y < (double) -this.stepHeight && (vec3d4 = Entity.adjustMovementForCollisions((Entity) (Object) this, new Vec3d(movement.x, 0.0, movement.z), box.offset(vec3d3), this.world, list).add(vec3d3)).horizontalLengthSquared() > vec3d2.horizontalLengthSquared()) {
-                     vec3d2 = vec3d4;
-                 }
-                 if (vec3d2.horizontalLengthSquared() > vec3d.horizontalLengthSquared()) {
-                     cir.setReturnValue( vec3d2.add(Entity.adjustMovementForCollisions((Entity) (Object) this, new Vec3d(0.0, -vec3d2.y + movement.y, 0.0), box.offset(vec3d2), this.world, list)));
-                 }
-             }
-             cir.setReturnValue( vec3d);
-         }
+    public void adjustMovementForCollisions(Vec3d movement, CallbackInfoReturnable<Vec3d> cir) {
+        //ExampleMod.LOGGER.info(this.upsideDownGravity());
+        if (this.upsideDownGravity()) {
+            boolean bl4;
+            Box box = this.getBoundingBox();
+            List<VoxelShape> list = this.world.getEntityCollisions((Entity) (Object) this, box.stretch(movement));
+            Vec3d vec3d = movement.lengthSquared() == 0.0 ? movement : Entity.adjustMovementForCollisions((Entity) (Object) this, movement, box, this.world, list);
+            boolean bl = movement.x != vec3d.x;
+            boolean bl2 = movement.y != vec3d.y;
+            boolean bl3 = movement.z != vec3d.z;
+            boolean bl5 = bl4 = this.onGround || bl2 && movement.y > 0.0;
+            if (this.stepHeight > 0.0f && bl4 && (bl || bl3)) {
+                Vec3d vec3d4;
+                Vec3d vec3d2 = Entity.adjustMovementForCollisions((Entity) (Object) this, new Vec3d(movement.x, -this.stepHeight, movement.z), box, this.world, list);
+                Vec3d vec3d3 = Entity.adjustMovementForCollisions((Entity) (Object) this, new Vec3d(0.0, -this.stepHeight, 0.0), box.stretch(movement.x, 0.0, movement.z), this.world, list);
+                if (vec3d3.y > (double) this.stepHeight && (vec3d4 = Entity.adjustMovementForCollisions((Entity) (Object) this, new Vec3d(movement.x, 0.0, movement.z), box.offset(vec3d3), this.world, list).add(vec3d3)).horizontalLengthSquared() > vec3d2.horizontalLengthSquared()) {
+                    vec3d2 = vec3d4;
+                }
+                if (vec3d2.horizontalLengthSquared() > vec3d.horizontalLengthSquared()) {
+                    cir.setReturnValue( vec3d2.add(Entity.adjustMovementForCollisions((Entity) (Object) this, new Vec3d(0.0, -vec3d2.y + movement.y, 0.0), box.offset(vec3d2), this.world, list)));
+                    cir.cancel();
+                }
+            }
+            cir.setReturnValue( vec3d);
+            cir.cancel();
+        }
+        //ExampleMod.LOGGER.info("continued");
     }
+
 
 
     /**
